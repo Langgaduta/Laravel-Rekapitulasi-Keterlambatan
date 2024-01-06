470cc11df8503a550022d2d2b8fedf8096eb127b
Langgaduta <lazzuardilanggadutawijaya@smkwikrama.sch.id>
2024-01-06 20:03:50 +0700

    first commit

---
+++ app/Http/Middleware/isGuest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class isGuest
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle($request, Closure $next)
+    {
+        if (Auth::check()) {
+            $role = Auth::user()->role;
+
+            switch ($role) {
+                case 'admin':
+                    return redirect('/');
+                    break;
+                case 'ps':
+                    return redirect('/dashboardPs');
+                    break;
+                default:
+                    return redirect('/');
+            }
+        }
+
+        return $next($request);
+    }
+}
